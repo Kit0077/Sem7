a27f7d80ea91fbe6c29ae5936da3c1ab1f77c7e9
RpPrime <NikitaMatveev007@gmail.com>
2023-08-31 16:01:26 +0300

    ask 52 is completed

---
+++ Sem7.3/Program.cs
@@ -0,0 +1,45 @@
+﻿int[,] GetArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = new Random().Next(1, 7);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void average(int[,] arr)
+{
+    
+    int count = 0;
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        double sum = 0;
+        count++;
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            sum += arr[j, i];
+        }
+        Console.WriteLine($"Ср.арифметическое столбца №{count} = {sum / arr.GetLength(0)}");
+    }
+    
+}
+
+int[,] array = GetArray(3, 5);
+PrintArray(array);
+average(array);
