47e2723fa6a86fda3c33e3ac59ec7abe0edbc638
RpPrime <NikitaMatveev007@gmail.com>
2023-08-31 12:02:05 +0300

    Task 47 is completed

---
+++ Sem7.1/Program.cs
@@ -0,0 +1,27 @@
+﻿double[,] GetArray(int m, int n)
+{
+    double[,] arr = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = new Random().NextDouble();
+        }
+    }
+    return arr;
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = GetArray(5, 5);
+PrintArray(array);
